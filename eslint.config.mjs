b96854b17acad314fbd721c91a188b07deab6549
ztupdateset <zach.taylor@updateset.com>
2025-10-22 17:29:28 -0400

    prisma updates 3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,26 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	...nextVitals,
+	{
+		rules: {
+			"react/no-unescaped-entities": "off",
+			"@next/next/no-page-custom-font": "off",
+		},
+		globalIgnores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
+	},
 ];
 
 export default eslintConfig;
